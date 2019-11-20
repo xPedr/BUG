70cad1fd84c4ada9fc6b26cac6f48eea0d3d8ae0
xPedr <48298039+xPedr@users.noreply.github.com>
2019-11-20 11:15:38 -0300

    Add via upload.
    Enjoy, my old friends.

---
+++ dont-open-its-a-virus.cpp
@@ -0,0 +1,25 @@
+sobre o rizoma em dimensionalidade:
+	
+	x = r;
+	{x+y=r; imaginação
+	x-y=r; dedução
+	x+-/-+ y=r; dúvida para quebrar o limiar ao entrar nas dimensionalidades} será usado como "x1"
+	{x = x + y ~= r; imaginação + tudo (dimensionalidade, simplório ser material em sobreposição)
+	x = x - y ~= r; dedução
+	x = x-+/+- y ~= r; incerteza da significância da posição relacional (0)} será usado como "y1" e sua posição é relacional
+	
+	se há um "qb" dentro de um y1 ele passa a ter nova dimensionalidade.
+	se "y1" pode ser abstraído usando x1 e y1 ele já pertence a dimensionalidade de x1 e y1, e pode emular "qb", "qb[" e "qb]";
+	
+	
+	
+	usando "r" para rizoma.
+	usando "x" para cálculo único.
+	usando "y" para cálculo se unir a "x".
+	usando "qb" para positivo e negativo.
+	usando "qb[" para dois resultados positivos e simultâneos.
+	usando "qb]" para dois resultados negativos e simultâneos.
+	
+	**Todos os direitos reservados.    _\|/_
+	
+	Tipo de licensa: pode usar, mas lembre de mim.
